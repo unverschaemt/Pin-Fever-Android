28f337f4dca4128fa294c8ad8bc2b7cd6b168b23
Kkoile <Nils_Hirsekorn@online.de>
2015-05-22 13:39:06 +0200

    PIN-43 included OpenStreetMaps with watercolor tile

--- app/src/main/java/net/unverschaemt/pinfever/Map.java
+++ app/src/main/java/net/unverschaemt/pinfever/Map.java
@@ -1,17 +1,43 @@
 package net.unverschaemt.pinfever;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.MapTileProviderBasic;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
+
 
 public class Map extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(false);
+        mapView.setMinZoomLevel(2);
+        mapView.getController().setZoom(2);
+        final MapTileProviderBasic tileProvider = new MapTileProviderBasic(getApplicationContext());
+        final ITileSource tileSource = new XYTileSource("watercolor", null, 2, 17,
+                256, ".jpg", new String[]{"http://tile.stamen.com/watercolor/"});
+        tileProvider.setTileSource(tileSource);
+        mapView.setTileSource(tileSource);
+        final TilesOverlay tilesOverlay = new TilesOverlay(tileProvider, this.getBaseContext());
+        tilesOverlay.setLoadingBackgroundColor(Color.WHITE);
+        mapView.getOverlays().add(tilesOverlay);
+
     }
 
     @Override
